fab283bedc858dd344799401255721c3d5004926
Ewerton Tavares <ewertont@users.noreply.github.com>
2021-09-15 01:35:49 -0300

    update

--- src/app/Home/home.jsx
+++ src/app/Home/home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import Navbar from '../Components/Navbar/navbar';
-import './home.css';
+import React from "react";
+import Navbar from "../Components/Navbar/navbar";
+import ListaClientes from "../Components/ListaCliente/listacliente";
+import "./home.css";
 
-function Home(){
-    return <div>
-      <Navbar/>
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div className="titulo">
+        <h1>Cadastro de Clientes</h1>
+        <ListaClientes/>
+      </div>
     </div>
-  }
+  );
+}
 
 export default Home;
