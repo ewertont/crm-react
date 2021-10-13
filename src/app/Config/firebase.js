ed90b12847dbf5f6753439dc5591b90e540e98aa
Ewerton Tavares <ewertont@users.noreply.github.com>
2021-10-13 17:08:01 -0300

    update

--- src/app/Config/firebase.js
+++ src/app/Config/firebase.js
@@ -9,7 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "1012763417803",
   appId: "1:1012763417803:web:9e7409e1beba9d87068f4e",
   measurementId: "G-M5NZ4X4PE4"
-};
-  
-  // Initialize Firebase
+}; 
+
 export default firebase.initializeApp(firebaseConfig);
